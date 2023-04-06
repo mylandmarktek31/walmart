e2a9343f12a6fb522733b36ad2291fc618f7ce5b
awoyinfa <awoyinfa@live.com>
2023-04-06 05:48:26 +0000

    git history class31 hands-on

---
+++ git-history.sh
@@ -0,0 +1,39 @@
+[christina@git paypal]$ history
+    1  sudo hostnamectl set-hostname git
+    2  sudo adduser christina && passwd
+    3  sudo visudo
+    4  sudo su - christina
+    1  mkdir paypal && cd paypal
+    2  sudo yum install update vim git -y
+    3  sudo yum install update -y
+    4  sudo yum install vim -y
+    5  sudo yum install git -y
+    6  clear
+    7  git init
+    8  git remote add origin https://github.com/Christ-christy-Cyber-Caffee/paypal
+    9  11
+   10  git remote add sale  https://github.com/Christ-christy-Cyber-Caffee/paypal
+   11  ll
+   12  git branch
+   13  git pull https://github.com/Christ-christy-Cyber-Caffee/paypal
+   14  ll
+   15  git config --global user.email "christina351973@gmail.com"
+   16  git config --global user.name "NkeTecque"
+   17  git config --global --list
+   18  git remote -v
+   19  vi userMgt.java
+   20  clear
+   21  git status
+   22  git add .
+   23  git status
+   24  git branch
+   25  git commit -m "I must succeed in this commit"
+   26  git push pay master
+   27  vi note.txt
+   28  clear
+   29  git status
+   30  git add .
+   31  git status
+   32  git commit -m "second commit"
+   33  git push pay master
+   34  history
